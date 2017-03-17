4909e37ea78bd8b25567ab40ff73b95028387203
lfzyx <lfzyx.me@gmail.com>
2017-03-17 16:13:32 +0800

    Use wtforms SelectField and  choose dynamic values from jenkins server

--- ButterSalt/views/deployment.py
+++ ButterSalt/views/deployment.py
@@ -2,16 +2,17 @@ from flask import Blueprint, flash, redirect, url_for, request
 from flask_login import login_required
 from flask import render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
+from wtforms import StringField, SubmitField, TextAreaField, SelectField
 from wtforms.validators import InputRequired
+import jenkins
 from ButterSalt import J_server, db
 from ButterSalt.models import JenkinsJobs
 from pathlib import Path
 import re
 
 
-class Job(FlaskForm):
-    job = StringField('job', validators=[InputRequired('名称是必填的')])
+class Application(FlaskForm):
+    application = SelectField('添加可用Application')
     submit = SubmitField('添加')
 
 
@@ -31,24 +32,26 @@ deployment = Blueprint('deployment', __name__, url_prefix='/deployment')
 @deployment.route('/operation/', methods=['GET', 'POST'])
 @login_required
 def operation():
-    jobs = dict()
-    for n in JenkinsJobs.query.all():
-        jobs[n.job_name] = J_server.get_job_info(n.job_name)['lastSuccessfulBuild']['number']
-    return render_template('deployment/operation.html', jobs=jobs)
-
-
-@deployment.route('/operation/add', methods=['GET', 'POST'])
-@login_required
-def operation_add():
-    form = Job()
+    jobs = J_server.get_jobs()
+    l = list()
+    for n in jobs:
+        l.append(tuple([n['name'], n['fullname']]))
+    form = Application()
+    form.application.choices = l
     if form.validate_on_submit():
-        job = form.job.data
-        execute = JenkinsJobs(job, None)
+        application = form.application.data
+        execute = JenkinsJobs(application, None)
         db.session.add(execute)
         db.session.commit()
         flash('添加成功')
         return redirect(url_for('deployment.operation'))
-    return render_template('deployment/operation_add.html', form=form)
+    jobs = dict()
+    for n in JenkinsJobs.query.all():
+        try:
+            jobs[n.job_name] = J_server.get_job_info(n.job_name)['lastSuccessfulBuild']['number']
+        except jenkins.NotFoundException as err:
+            print('jenkins exception: {0}'.format(err))
+    return render_template('deployment/operation.html', jobs=jobs, form=form)
 
 
 @deployment.route('/operation/deployconfig/',  methods=['GET', 'POST'])
